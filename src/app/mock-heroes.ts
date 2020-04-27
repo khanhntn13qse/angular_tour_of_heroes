a48c79a4d0868c7177b39ebabd54806872900524
khanhntn13it <55081954+khanhntn13it@users.noreply.github.com>
2020-04-27 16:50:44 +0700

    Step4: add service
    - Refactored data access to the HeroService class.
    - Registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app.
    - Used Angular Dependency Injection to inject it into a component.
    You gave the HeroService get data method an asynchronous signature.
    - Discovered Observable and the RxJS Observable library.
    - Used RxJS of() to return an observable of mock heroes (Observable<Hero[]>).
    - The component's ngOnInit lifecycle hook calls the HeroService method, not the constructor.
    - Created a MessageService for loosely-coupled communication between classes.
    - The HeroService injected into a component is created with another injected service, MessageService.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Pikachu'},
+    {id: 12, name: 'Tokepi'},
+    {id: 13, name: 'Shaymin'},
+    {id: 14, name: 'Rokon'},
+    {id: 15, name: 'Kyukon'},
+    {id: 16, name: 'Mokoko'},
+    {id: 17, name: 'Denryu'},
+    {id: 18, name: 'Viviyon'},
+]
